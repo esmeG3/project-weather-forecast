6e8b649aae3c9e63620b636a38a39db45fb38482
esG3 <esmeG3@users.noreply.github.com>
2024-04-08 12:39:24 +0100

    added footer with links

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App
- 
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/esmeG3" target="_blank" rel="noreferrer">
+          Esme G
+        </a>
+        , is open-sourced on{" "}
+        <a
+          href="https://github.com/esmeG3/project-weather-forecast"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://forecast-theweather.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netfliy
+        </a>
+      </footer>
     </div>
   );
 }

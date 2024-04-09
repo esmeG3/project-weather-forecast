d647b702d4b0545eb098c480af142fa8e8cc4c8b
esG3 <esmeG3@users.noreply.github.com>
2024-04-09 12:23:04 +0100

    added weather.js

---
+++ Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <h1>Weather </h1>
+        </div>
+    );
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
+      <Weather/>
       <footer>
         This project was coded by{" "}
         <a href="https://github.com/esmeG3" target="_blank" rel="noreferrer">
@@ -27,6 +30,7 @@ export default function App() {
           Netfliy
         </a>
       </footer>
+      </div>
     </div>
   );
 }

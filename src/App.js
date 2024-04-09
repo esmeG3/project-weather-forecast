fc6fdaeceeef554dc2c9028ebd644241a93131ea
esG3 <esmeG3@users.noreply.github.com>
2024-04-09 13:05:21 +0100

    ensured bootstrap was running

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App
       <Weather/>
       <footer>
         This project was coded by{" "}

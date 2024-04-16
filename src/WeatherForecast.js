05e547df675e8739ad6f9f9a14e12e907840872f
esG3 <esmeG3@users.noreply.github.com>
2024-04-16 20:36:12 +0100

    added use effect to fix loading bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
 let [loaded, setLoaded] = useState(false);
-
 let [forecast, setForecast] = useState(null);
+
+useEffect(() => {
+setLoaded(false);
+}, [props.coordinates]);
+
+
 
     function handleResponse(response) {
         setForecast(response.data.daily);
@@ -21,7 +26,7 @@ return (
         {forecast.map(function(dailyForecast, index){
             if (index < 5) {     
             return (
-                
+
         <div className="col" key={index}>
           <WeatherForecastDay data={dailyForecast} /> 
         </div>
